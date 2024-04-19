907cb27ee7c212f7cc064d27da6207fb98b6b8c9
cruzz05 <cruzkaua917@gmail.com>
2024-04-19 16:03:33 -0300

    Initial commit

---
+++ exercicio03.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*ATIVIDADE 1*/
+int main () {
+			/*numero1 = float(input("Informe o primeiro número: "))
+
+			numero2 = float(input("Informe o segundo número: "))
+
+			if numero1 > numero2{
+				print("O primeiro número é maior.")
+			}
+
+			elif numero2 > numero{
+				print("O segundo número é maior.")
+			}
+
+
+			else {
+				print("Os números são iguais.")
+			}*/
+
+
+			/*ATIVIDADE 2*/
+
+		/*quantidade_produtos = int(input("Informe a quantidade de produtos no estoque: "))
+
+		if quantidade_produtos >= 100{
+			print("Quantidade suficiente no estoque.")};
+
+		elif quantidade_produtos >= 50{
+				print("Atenção ao estoque.")};
+
+		else {
+			print("Faça um novo pedido e reabasteça o estoque.")
+		};*/
+
+
+		/*ATIVIDADE 3*/
+
+
+	/*numero = float(input("Informe um número: "))
+
+
+		if numero > 0{
+			print("O número é positivo.")};
+
+		elif numero < 0 {
+			print("O número é negativo.")};
+
+		else {
+			print("O número é igual a zero.")
+		};*/
